589e0573916ee996d6ee86f533e72117c4471fd8
Abdulbasit Murtadho <murtadhoabdulbasit62@gmail.com>
2022-10-14 13:33:28 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowercase alphabet
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('z' - i);
+	}
+	putchar('\n');
+	return (0);
+}
