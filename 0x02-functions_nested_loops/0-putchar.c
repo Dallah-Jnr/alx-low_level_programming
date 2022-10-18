efaefe9810b761506b54793097807e8cf3e1675c
Abdulbasit Murtadho <murtadhoabdulbasit62@gmail.com>
2022-10-18 06:54:13 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * written by Dallah-Jnr
- * Return: 0
+ * main - Prints _putchar
+ * Return 0 if success
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
