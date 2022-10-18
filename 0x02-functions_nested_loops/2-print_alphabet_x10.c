bd3afc50e00f6de5741e99bd7dfec7179f663784
Abdulbasit Murtadho <murtadhoabdulbasit62@gmail.com>
2022-10-18 13:29:14 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,22 @@
 
 /**
  * print_alphabet_x10 - A function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	char c;
+	int n = 0;
 	
-	while (count++ <= 9)
+	for (; n < 10; n++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('/n');
-	}	
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
