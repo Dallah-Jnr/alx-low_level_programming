8527da459fd8602c735aa0d2672bb0682bd10360
Abdulbasit Murtadho <murtadhoabdulbasit62@gmail.com>
2022-10-24 07:25:37 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TARGET 2772
+#define MAX 126
+#define MIN 33
+
+/**
+ * main - generate keygen
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int checksum = 0;
+
+	srand( time( 0 ) );
+
+	while ( checksum + MAX <= TARGET )
+	{
+		char random_char = rand() % (MAX - MIN + 1) + MIN;
+
+		if (TARGET - (checksum + random_char) <= MIN)
+			continue;
+		checksum += (int) random_char;
+		printf("%c", random_char);
+	}
+	printf("%c", TARGET - checksum);
+
+	return (0);
+}
