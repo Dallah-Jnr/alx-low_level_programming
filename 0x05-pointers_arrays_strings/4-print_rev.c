bd9cc7ff800ebe7957c480d8a1c8a8850a963622
Abdulbasit Murtadho <murtadhoabdulbasit62@gmail.com>
2022-10-24 05:00:16 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int i;
-	char c;
+int i;
+char c;
 
-	for (i = 0; s[i] != 0; i++)
-	{
-		
-	}
-	for (i = i - 1; i >= 0; i--)
-	{
-		c = s[i];
-		_putchar(c);
-	}
-	_putchar('\n');
+for (i = 0; s[i] != 0; i++)
+{
+
+}
+for (i = i - 1; i >= 0; i--)
+{
+c = s[i];
+_putchar(c);
+}
+_putchar('\n');
 }
