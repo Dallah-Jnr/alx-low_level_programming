e40bf5014d2220dd5f1778a8351ad25ddd2035e0
Abdulbasit Murtadho <murtadhoabdulbasit62@gmail.com>
2022-10-31 06:14:42 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memcpy - a function that copies memory area
  * @dest: char
@@ -11,7 +10,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 		
-	for (i = 0; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
